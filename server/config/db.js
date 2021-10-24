cad1d969443f82b2b3c7f131294e988d83511778
Charlie <charlie_bi@hotmail.com>
2021-10-23 22:08:48 -0400

    linked to online mongodb Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 module.exports = 
 {
     //references the contacts database in mongoDB
-    "URI": "mongodb://localhost/business_contacts"
+    //"URI": "mongodb://localhost/business_contacts"
+    "URI": "mongodb+srv://hbi08:26RMgKLTx1IBnB0L@mongodbserver.1y8br.mongodb.net/business_contacts?retryWrites=true&w=majority"
 }
